9ad0379ae71e9ca26df7950e93e1d65cf6d42857
Henrique Mauri <hgmauri@gmail.com>
2021-11-22 09:50:31 -0300

    update MessageResult

--- src/Sample.FluentValidation.WebApi.Core/MessageResult.cs
+++ src/Sample.FluentValidation.WebApi.Core/MessageResult.cs
@@ -2,12 +2,12 @@
 
 public class MessageResult
 {
+    public string Code { get; set; }
+    public string Description { get; set; }
+
     public MessageResult(string code, string description)
     {
         Code = code;
         Description = description;
     }
-
-    public string Code { get; set; }
-    public string Description { get; set; }
 }
